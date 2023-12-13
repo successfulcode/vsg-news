5954c84d9ab8b433ce9218519b7074af3bdd8c8f
Sergej Mickevic <Sergej@successfulcode.lan>
2023-12-13 22:29:28 +0200

    Added create news route

---
+++ app/[lang]/create-news/page.tsx
@@ -0,0 +1,5 @@
+function page() {
+  return <div>Create news</div>;
+}
+
+export default page;
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -7,6 +7,7 @@ function Navbar() {
   const navLinks = [
     {name: 'About', href: '/about'},
     {name: 'News', href: '/news'},
+    {name: 'Create news', href: '/create-news'},
     {name: 'Chat', href: '/chat'}
   ];
 
@@ -29,7 +30,8 @@ function Navbar() {
               <NavDrawer navLinks={navLinks} />
             </div>
             <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-              <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 
+                rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 {navLinks.map(
                   navLink => <li key={navLink.name}>
                     <NavLink href={navLink.href} key={navLink.name}>
