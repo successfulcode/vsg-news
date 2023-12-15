42fc550a13d88f54070abffc133d2153bbd66534
Sergej Mickevic <Sergej@successfulcode.lan>
2023-12-15 20:30:08 +0200

    Created database

---
+++ scripts/initdb.js
@@ -0,0 +1,48 @@
+const sql = require('better-sqlite3');
+const db = sql('./db/articles.db');
+
+const initArticles = [
+  {
+    slug: 'test slug',
+    title: 'test title',
+    image: '/images/test.jpg',
+    article: `test  arcticle`,
+    creator: 'test creator',
+    creator_email: 'test@test.com',
+    date: '2023-12-14'
+  }
+];
+
+db.prepare(`
+   CREATE TABLE IF NOT EXISTS meals (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      slug TEXT NOT NULL UNIQUE,
+      title TEXT NOT NULL,
+      image TEXT NOT NULL,
+      article TEXT NOT NULL,
+      creator TEXT NOT NULL,
+      creator_email TEXT NOT NULL,
+      date TEXT NOT NULL
+    )
+`).run();
+
+async function initData() {
+  const stmt = db.prepare(`
+      INSERT INTO meals VALUES (
+        null,
+        @slug,
+        @title,
+        @image,
+        @article,
+        @creator,
+        @creator_email,
+        @date
+      )
+   `);
+
+  for (const article of initArticles) {
+    stmt.run(article);
+  }
+}
+
+initData();
